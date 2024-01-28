aa052890d46cfb9d4eba88cb5fd162872d6cb0fc
preety thakur <preetythakur1992@gmail.com>
2024-01-28 02:19:08 -0500

    positioning

--- src/components/Mainvideo/Mainvideo.js
+++ src/components/Mainvideo/Mainvideo.js
@@ -1,7 +1,4 @@
 import React from 'react';
-import playIcon from '../../assets/Icons/play.svg';
-import fullscreenIcon from '../../assets/Icons/fullscreen.svg';
-import soundIcon from '../../assets/Icons/volume_off.svg';
 import "./MainVideo.scss";
 
 const MainVideo = ({ videoDetails }) => {
